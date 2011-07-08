a572321fc114c6396bab2e93ab4221fbe58dc27b
Felipe Coury <felipe.coury@gmail.com>
2011-07-07 20:23:26 -0400

    Some passenger/nginx fixes

--- cookbooks/passenger/attributes/passenger.rb
+++ cookbooks/passenger/attributes/passenger.rb
@@ -3,7 +3,7 @@ default.passenger[:nginx][:passenger_version] = "3.0.7"
 default.passenger[:nginx][:nginx_version] = "1.0.4"
 
 default.passenger[:root_path]        = "#{node[:languages][:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
-default.passenger[:enterprise_root_path] = "#{node[:languages][:ruby][:gems_dir]}/gems/passenger-enterprise-server-#{passenger[:nginx][:passenger_version]}"
+default.passenger[:enterprise_root_path] = "#{node[:languages][:ruby][:gems_dir]}/gems/passenger-#{passenger[:nginx][:passenger_version]}"
 
 default.passenger[:module_path]      = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
 default.passenger[:apache_load_path] = '/etc/apache2/mods-available/passenger.load'
