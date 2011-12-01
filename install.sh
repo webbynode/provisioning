09a2e42954fbcd87046637eeb035665c2c19ea0d
Felipe Coury <felipe.coury@gmail.com>
2011-12-01 20:50:56 -0200

    Added build-essential package.

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ fi
 
 if ! test -f "$chef_binary"; then
   aptitude install -y gcc
-  aptitude install -y libshadow-ruby1.8
+  aptitude install -y libshadow-ruby1.8 build-essential
   gem install --no-rdoc --no-ri chef --version 0.10.2
   gem install --no-rdoc --no-ri ruby-shadow
 fi
